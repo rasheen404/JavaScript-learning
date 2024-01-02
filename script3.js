2b08dd93aa28b756f88b49573288946fb534ebf5
Rasheen <rasheenerror404@gmail.com>
2024-01-02 12:04:09 +0530

    Learned about Arithmetic operator,Augmented Assignment Operator,Increment/Decrement Operator and Operator Presedence

---
+++ script3.js
@@ -0,0 +1,66 @@
+/*
+Arithmatic Operators
+(+,-,*,/)
+
+*/
+
+//Addition
+let class1 = 31;
+class1 = class1 + 1;
+console.log(class1);
+
+//Substraction
+let class2 = 31;
+class2 = class2 - 1;
+console.log(class2);
+
+//Multiplication
+let class3 = 31;
+class3 = class3 * 1;
+console.log(class3);
+
+//Division
+let class4 = 30;
+class4 = class4 / 2;
+console.log(class4);
+
+//Modulus
+let class5 = 31;
+class5 = class5 % 2;
+console.log(class5);
+
+//Power
+let class6 = 30;
+class6 = class6 ** 2;
+console.log(class6);
+
+//Augmented Assignment Operator
+
+/*
+let student = 30;
+student += 2;
+student -= 2;
+student *= 2;
+student /= 2;
+student %= 2;
+student **= 2;
+*/
+
+//Increment/Decrement Operator.
+
+/*
+student++;
+student--;
+*/
+
+//Operator Presedence
+
+/*
+1.parenthesis()
+2.exponents
+3.multiplication & division & modulus
+4.addition & substraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(`1 + 2 * 3 + 4 ** 2 = ${result}`);
